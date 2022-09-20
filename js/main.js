8b4a907ac99516bb8d00014ec7c5781c80244722
Zudikhin <uzudihin@gmail.com>
2022-09-20 18:30:07 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    "use strict";
+    
+    $(".header_block_content_drop").click(function() {
+        $(this).toggleClass("active");
+        $(".header").toggleClass("active");
+    });
+
+
+
+    $('.people_block_slider').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+            breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+    $('.explore_slider').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+            breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+});
